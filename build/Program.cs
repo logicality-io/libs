da69721d299689c540e028d22c5bf778441a9526
Damian Hickey <dhickey@gmail.com>
2021-03-27 23:57:26 +0100

    Adjust build to work with multiple projects dynamically

--- build/Program.cs
+++ build/Program.cs
@@ -1,64 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using static Bullseye.Targets;
 using static SimpleExec.Command;
 
-namespace Build
+const string artifactsDir = "artifacts";
+const string clean = "clean";
+const string build = "build";
+const string publish = "publish";
+
+Target(clean, () =>
 {
-    class Program
+    if (Directory.Exists(artifactsDir))
     {
-        private const string ArtifactsDir = "artifacts";
-        private const string Clean = "clean";
-        private const string Build = "build";
-        private const string Test = "test";
-        private const string Pack = "pack";
-        private const string Publish = "publish";
-
-        static void Main(string[] args)
-        {
-            Target(Clean, () =>
-            {
-                if (Directory.Exists(ArtifactsDir))
-                {
-                    Directory.Delete(ArtifactsDir, true);
-                }
-                Directory.CreateDirectory(ArtifactsDir);
-            });
-
-            Target(Build, () => Run("dotnet", "build Hosting.sln -c Release"));
-
-            Target(
-                Test,
-                DependsOn(Build),
-                () => Run("dotnet", $"test test/Hosting.Tests/Hosting.Tests.csproj -c Release -r {ArtifactsDir} --no-build -l trx;LogFileName=Hosting.Tests.xml --verbosity=normal"));
-
-            Target(
-                Pack,
-                DependsOn(Build),
-                new[] { "Hosting", "Hosting.Docker", "Hosting.SerilogConsoleLogging" },
-                project => Run("dotnet", $"pack src/{project}/{project}.csproj -c Release -o {ArtifactsDir} --no-build"));
-
-            Target(Publish, DependsOn(Pack), () =>
-            {
-                var packagesToPush = Directory.GetFiles(ArtifactsDir, "*.nupkg", SearchOption.TopDirectoryOnly);
-                Console.WriteLine($"Found packages to publish: {string.Join("; ", packagesToPush)}");
-
-                var apiKey = Environment.GetEnvironmentVariable("FEEDZ_LOGICALITY_API_KEY");
-                if (string.IsNullOrWhiteSpace(apiKey))
-                {
-                    Console.WriteLine("Feedz API Key not available. No packages will be pushed.");
-                    return;
-                }
-                Console.WriteLine($"Feedz API Key ({apiKey.Substring(0, 5)}) available. Pushing packages to Feedz...");
-                foreach (var packageToPush in packagesToPush)
-                {
-                    Run("dotnet", $"nuget push {packageToPush} -s https://f.feedz.io/logicality/public/nuget/index.json -k {apiKey} --skip-duplicate", noEcho: true);
-                }
-            });
-
-            Target("default", DependsOn(Clean, Test, Publish));
-
-            RunTargetsAndExit(args);
-        }
+        Directory.Delete(artifactsDir, true);
     }
+    Directory.CreateDirectory(artifactsDir);
+});
+
+Target(build, () => Run("dotnet", "build libs/hosting/Hosting.sln -c Release"));
+
+var projects = new[] {"configuration", "hosting"};
+var defaultTargets = new List<string>()
+{
+    clean
+};
+
+foreach (var project in projects)
+{
+    var sln = Directory.GetFiles($"libs/{project}/", "*.sln").Single();
+    var buildTarget = $"{project}-build";
+    Target(buildTarget, () => Run("dotnet", $"build {sln} -c Release"));
+    defaultTargets.Add(buildTarget);
+
+    var testTarget = $"{project}-test";
+    Target(testTarget, DependsOn(buildTarget),
+        () => Run("dotnet",
+            $"test {sln} -c Release --no-build -r {artifactsDir} " +
+            $"--collect:\"XPlat Code Coverage\" --settings build/coverlet-settings.xml"));
+    defaultTargets.Add(testTarget);
+
+    var packableProjects = Directory.GetFiles($"libs/{project}/src/", "*.csproj", SearchOption.AllDirectories);
+    var packTarget = $"{project}-pack";
+    Target(packTarget, DependsOn(buildTarget),
+        packableProjects,
+        packableProject => Run("dotnet", $"pack {packableProject} -c Release -o {artifactsDir} --no-build"));
+    defaultTargets.Add(packTarget);
 }
+
+Target(publish, () =>
+{
+    var packagesToPush = Directory.GetFiles(artifactsDir, "*.nupkg", SearchOption.TopDirectoryOnly);
+    Console.WriteLine($"Found packages to publish: {string.Join("; ", packagesToPush)}");
+
+    var apiKey = Environment.GetEnvironmentVariable("FEEDZ_LOGICALITY_API_KEY");
+    if (string.IsNullOrWhiteSpace(apiKey))
+    {
+        Console.WriteLine("Feedz API Key not available. No packages will be pushed.");
+        return;
+    }
+    Console.WriteLine($"Feedz API Key ({apiKey.Substring(0, 5)}) available. Pushing packages to Feedz...");
+    foreach (var packageToPush in packagesToPush)
+    {
+        Run("dotnet", $"nuget push {packageToPush} -s https://f.feedz.io/logicality/public/nuget/index.json -k {apiKey} --skip-duplicate", noEcho: true);
+    }
+});
+
+defaultTargets.Add(publish);
+Target("default", DependsOn(defaultTargets.ToArray()));
+
+RunTargetsAndExit(args);
