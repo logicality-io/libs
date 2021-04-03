27b7aae51aea811890bda43b24dc1412e26310f1
Damian Hickey <dhickey@gmail.com>
2021-04-03 15:18:59 +0200

    File download utility

---
+++ libs/system-extensions/src/SystemExtensions/Net/Http/FileDownload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Logicality.SystemExtensions.Net.Http
+{
+    public static class FileDownload
+    {
+        public static async Task DownloadFile(
+            this HttpMessageInvoker httpMessageInvoker,
+            Uri source,
+            string filePath,
+            bool overwrite = true,
+            CancellationToken cancellationToken = default)
+        {
+            if (File.Exists(filePath) && !overwrite)
+            {
+                return;
+            }
+
+            using var request = new HttpRequestMessage(HttpMethod.Get, source);
+            var responseMessage = await httpMessageInvoker.SendAsync(request, cancellationToken);
+            responseMessage.EnsureSuccessStatusCode();
+            await using var contentStream = await responseMessage.Content.ReadAsStreamAsync();
+            await using var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+            await contentStream.CopyToAsync(fileStream, cancellationToken);
+        }
+    }
+}
---
+++ libs/system-extensions/test/SystemExtensions.Tests/Net/Http/FileDownloadTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Logicality.SystemExtensions.Net.Http;
+using Shouldly;
+using Xunit;
+
+namespace Logicality.System.Net.Http
+{
+    public class DownloadFileTests
+    {
+        [Fact]
+        public async Task Can_download_file()
+        {
+            var client = new HttpClient();
+
+            var sourceUri = new Uri("https://google.com/robots.txt");
+            var destinationPath = $"{Path.GetTempPath()}{Guid.NewGuid()}.txt";
+            await client.DownloadFile(sourceUri, destinationPath);
+
+            File.Exists(destinationPath).ShouldBeTrue();
+        }
+    }
+}
