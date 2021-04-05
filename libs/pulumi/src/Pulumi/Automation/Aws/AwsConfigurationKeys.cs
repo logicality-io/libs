48314d0fdbc455e46678a9638b4aeb1d8ce8e47f
Damian Hickey <dhickey@gmail.com>
2021-04-05 20:27:40 +0200

    Add endpoints config key

--- libs/pulumi/src/Pulumi/Automation/Aws/AwsConfigurationKeys.cs
+++ libs/pulumi/src/Pulumi/Automation/Aws/AwsConfigurationKeys.cs
@@ -7,5 +7,6 @@
         public const string SecretKey = "aws:secretKey";
         public const string SkipCredentialsValidation = "aws:skipCredentialsValidation";
         public const string SkipRequestingAccountId = "aws:skipRequestingAccountId";
+        public const string Endpoints = "aws:endpoints";
     }
 }
