27f990b22c9ed2328c934f0bb91bba8ded3667f6
Damian Hickey <dhickey@gmail.com>
2021-07-18 09:55:07 +0200

    refactor workflows

---
+++ .github/workflows/gen-workflows.ps1
@@ -0,0 +1,9 @@
+# Generates a workflow yml file for each library using the template
+
+$libs = @("aspnet-core", "bullseye", "configuration", "hosting", "lambda", "pulumi", "system-extensions", "testing")
+
+$libs |ForEach-Object {
+    $workflow = "$_-ci.yml"
+    Copy-Item -Path lib.yaml.template -Destination $workflow -Force
+    ((Get-Content -Path $workflow -Raw) -replace "<lib>", "$_" ) | Set-Content -Path $workflow
+}
