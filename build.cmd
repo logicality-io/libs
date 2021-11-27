97b58aec62c155417c99428f87d0b3148761dc8f
Damian Hickey <dhickey@gmail.com>
2021-11-27 20:17:26 +0100

    rename the build containers

--- build.cmd
+++ build.cmd
@@ -3,14 +3,14 @@
 :: Builds the build environment image.
 docker build ^
  -f build.dockerfile ^
- --tag dotnet-libs-build .
+ --tag logicality-platform-libs-build .
 
 if errorlevel 1 (
    echo Docker build failed: Exit code is %errorlevel%
    exit /b %errorlevel%
 )
 
-docker run --rm -it --name dotnet-libs-build ^
+docker run --rm -it --name logicality-platform-libs-build ^
  -v /var/run/docker.sock:/var/run/docker.sock ^
  -v %cd%/artifacts:/repo/artifacts ^
  -v %cd%/.git:/repo/.git ^
@@ -19,7 +19,7 @@ docker run --rm -it --name dotnet-libs-build ^
  -e FEEDZ_LOGICALITY_API_KEY=%FEEDZ_LOGICALITY_API_KEY% ^
  -e BUILD_NUMBER=%GITHUB_RUN_NUMBER% ^
  --network host ^
- dotnet-libs-build ^
+ logicality-platform-libs-build ^
  dotnet run -p build/Build.csproj -c Release -- %*
 
 if errorlevel 1 (
--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ set -e
 
 docker build \
  -f build.dockerfile \
- --tag dotnet-libs-build .
+ --tag logicality-platform-libs-build .
 
-docker run --rm --name hosting-build \
+docker run --rm --name logicality-platform-libs-build \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v $PWD/artifacts:/repo/artifacts \
  -v $PWD/.git:/repo/.git \
@@ -15,5 +15,5 @@ docker run --rm --name hosting-build \
  -e NUGET_PACKAGES=/repo/temp/nuget-packages \
  -e BUILD_NUMBER=$GITHUB_RUN_NUMBER \
  --network host \
- dotnet-libs-build \
+ logicality-platform-libs-build \
  dotnet run -p build/Build.csproj -c Release -- "$@"
