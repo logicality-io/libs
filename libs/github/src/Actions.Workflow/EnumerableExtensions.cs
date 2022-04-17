f8e5622b0fdf2a8e558219eaaa470167805c73c8
Damian Hickey <dhickey@gmail.com>
2022-04-17 11:56:28 +0200

    These don't belong here.

--- libs/github/src/Actions.Workflow/EnumerableExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Logicality.GitHub.Actions.Workflow;
-
-internal static class EnumerableExtensions
-{
-    internal static bool None<TSource>(this IEnumerable<TSource>? source) 
-        => source == null || !source.Any();
-}
