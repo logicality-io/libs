d10f7baa469a40ac86db4c6ed25cfc6386966a91
Damian Hickey <dhickey@gmail.com>
2023-04-02 19:04:40 +0200

    Add a snake casing naming policy for system text json

---
+++ libs/system-extensions/src/SystemExtensions/Text/Json/SnakeCaseNamingPolicy.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace Logicality.SystemExtensions.Text.Json;
+
+public class SnakeCaseNamingPolicy : JsonNamingPolicy
+{
+    public static SnakeCaseNamingPolicy Instance { get; } = new();
+
+    public override string ConvertName(string name)
+    {
+        return string.Concat(name.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())).ToLower();
+    }
+}
---
+++ libs/system-extensions/src/SystemExtensions/Text/Json/StringUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+
+namespace Logicality.SystemExtensions.Text.Json;
+
+internal static class StringUtils
+{
+    public static string ToSnakeCase(this string str)
+    {
+        return string.Concat(str.Select((x, i) => i > 0 && char.IsUpper(x) ? "_" + x : x.ToString())).ToLower();
+    }
+}
