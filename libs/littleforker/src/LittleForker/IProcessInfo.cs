129fe4766daec11343353d6540f76dda816bd375
Damian Hickey <dhickey@gmail.com>
2023-05-13 10:27:53 +0200

    Import LittleForker

---
+++ libs/littleforker/src/LittleForker/IProcessInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace LittleForker
+{
+    public interface IProcessInfo
+    {
+        /// <summary>
+        ///     The process's exit code.
+        /// </summary>
+        int ExitCode { get; }
+
+        /// <summary>
+        ///     The process's Id.
+        /// </summary>
+        int Id { get; }
+    }
+}
---
+++ libs/littleforker/tests/LittleForker.Tests/CooperativeShutdownTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Shouldly;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace LittleForker
+{
+    public class CooperativeShutdownTests
+    {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public CooperativeShutdownTests(ITestOutputHelper outputHelper)
+        {
+            _loggerFactory = new XunitLoggerFactory(outputHelper).LoggerFactory;
+        }
+
+        [Fact]
+        public async Task When_server_signals_exit_then_should_notify_client_to_exit()
+        {
+            var exitCalled = new TaskCompletionSource<bool>();
+            var listener = await CooperativeShutdown.Listen(
+                () => exitCalled.SetResult(true),
+                _loggerFactory);
+
+            await CooperativeShutdown.SignalExit(Process.GetCurrentProcess().Id, _loggerFactory);
+
+            (await exitCalled.Task).ShouldBeTrue();
+
+            listener.Dispose();
+        }
+    }
+}
