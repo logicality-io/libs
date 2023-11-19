da99aff492eec1fabc9db56cc62dfc4b5ace7177
Damian Hickey <dhickey@gmail.com>
2023-11-19 12:07:41 +0100

    Struct -> Record.

--- libs/eventsourcing-domain/src/EventSourcing.Domain/StreamName.cs
+++ libs/eventsourcing-domain/src/EventSourcing.Domain/StreamName.cs
@@ -1,19 +1,6 @@
 ﻿namespace Logicality.EventSourcing.Domain;
 
-public readonly struct StreamName : IEquatable<StreamName>
+public record StreamName(string Value)
 {
-    private readonly string _value;
-
-    public StreamName(string value)
-    {
-        _value = value ?? throw new ArgumentNullException(nameof(value));
-    }
-
-    public bool Equals(StreamName other) => string.Equals(_value, other._value, StringComparison.Ordinal);
-
-    public override bool Equals(object? obj) => obj is StreamName other && Equals(other);
-        
-    public override int GetHashCode() => _value == null ? 0 : _value.GetHashCode();
-
-    public override string ToString() => _value ?? "";
+    public override string ToString() => Value;
 }
